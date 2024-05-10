f35f3765e3b16c672223b5197980774d842d0b81
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-05-10 11:46:52 +0100

    e1.5 Refactor App and Header. Exercise complete
    - move parts array into course variable
    - change what was as props
    - refactor header as course.name is now passed as props, not course. so
    no longer need to specify course.name in Header.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   console.log(props);
   return (
     <>
-      <h1>{props.course.name}</h1>
+      <h1>{props.course}</h1>
     </>
   );
 };
@@ -45,27 +45,27 @@ const Total = (props) => {
 const App = () => {
   const course = {
     name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
